b77412a4899aaf063006f683424c6137ee58fd90
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-09-10 20:17:29 +0300

    Initial Commit

---
+++ RGB Slider SwiftUI/RGB_Slider_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGB_Slider_SwiftUIApp.swift
+//  RGB Slider SwiftUI
+//
+//  Created by Karpinets Alexander on 10.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGB_Slider_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
